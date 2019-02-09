988f00b747f827d2b5e45131a2e7429cfc5cfa72
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2019-02-09 15:06:27 -0500

    timgor branch dump

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
+import json
 import discord
-import os
 import asyncio
-from random import randint
 
+startup = True
 client = discord.Client()
 
-mode = 0
+with open('config.json', 'r') as read_file:
+    global config
+    config = json.load(read_file)
 
-#runs when started
-@client.event
-async def on_ready():
-    print("I'm in")
-    print(client.user)
 
-#runs when a message was sent
+# add each command as a static method to this class
+class commands:
+    @staticmethod
+    async def test(params, channel):
+        if params:
+            await channel.send('hello, {}!'.format(params[1]))
+        else:
+            await channel.send('hello, world!')
+
 @client.event
 async def on_message(message):
-    global mode
-    
-    #example message sorter
-    if message.content == "/lt mode sitcom":
-      #changes a global variable if this message
-      mode = 1
-    elif message.content == "/lt mode 0":
-      #changes a global variable if this message
-      mode = 0
-    elif mode == 1 and message.author.name != "Laugh Track":
-      #sends a message here
-      await client.send_message(message.channel,'\*laugh track\*')
-    elif message.content == "/lt":
-      #sends a message here
-      await client.send_message(message.channel,'\*laugh track\*')
-    elif message.content == "/It":
-      #sends a message here
-      await client.send_message(message.channel, 'its lowercase l, not an uppercase i')
+    # get params and run command
+    if message.content.startswith(config['trigger']):
+        content = message.content.replace(config['trigger'], '', 1).split(' ')
+        command = content[0]
+        params = []
+        if len(content) > 1:
+            params = content[1:]
+
+        await getattr(commands, command)(params, message.channel)
 
 
-client.run('#discord-bot-token-replace')
+@client.event
+async def on_ready():
+    global startup
+    if startup:
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+client.run(config['token'])
