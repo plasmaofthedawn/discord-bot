8a05b61013ace1282e27d29083338f7776e50a4c
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2019-02-10 16:30:19 -0500

    Revert "Add models to the database"

--- setup_database.py
+++ setup_database.py
@@ -1,82 +1,68 @@
 import sqlite3
-from models import *
-from timezone import TIMEZONES, DAYS
 
 db = sqlite3.connect('database.db')
 cursor = db.cursor()
 
-
 def remove_tables():
-    try:
-        cursor.execute('DROP TABLE intervals')
-    except (sqlite3.OperationalError, sqlite3.DatabaseError):
-        pass
-
-    try:
-        cursor.execute('DROP TABLE users')
-    except (sqlite3.OperationalError, sqlite3.DatabaseError):
-        pass
-
+	#attempt to drop tables IF they exist
+	try:
+ 	   cursor.execute('DROP TABLE users')
+	except:
+ 	   pass
+	try:
+ 	   cursor.execute('DROP TABLE interval')
+	except:
+ 	   pass
 
 def create_tables():
-    cursor.execute("""CREATE TABLE users (
-                            discord_id INTEGER PRIMARY KEY,
-                            timezone   INTEGER
-                    )""")
+	#create the tables
+	cursor.execute('CREATE TABLE users ('
+ 	              'discordId INTEGER,'
+  	             'timezone INTEGER)')
 
-    cursor.execute("""CREATE TABLE intervals (
-                            id         INTEGER PRIMARY KEY AUTOINCREMENT,
-                            user_id    INTEGER,
-                            day        INTEGER,
-                            start_hour INTEGER,
-                            end_hour   INTEGER,
-                            FOREIGN KEY(user_id) REFERENCES users(discord_id)
-                    )""")
+	cursor.execute('CREATE TABLE interval ('
+ 	              'id INTEGER,'
+  	             'userId INTEGER,'
+   	            'startDay INTEGER,'
+    	           'startHour DOUBLE,'
+     	          'endDay INTEGER,'
+      	         'endHour DOUBLE)'
+       	        )
 
+def add_user(discordId, timezone):
+	cursor.execute('INSERT INTO users (discordId, timezone) VALUES ('+
+								str(discordId) + ", " +
+								str(timezone) + ")")
+	db.commit()
 
-def add_user(discord_id, timezone):
-    try:
-        cursor.execute('INSERT INTO users  VALUES (?, ?)', (discord_id, timezone))
-        db.commit()
+def add_interval(id, userId, startDay, startHour, endDay, endHour):
+	cursor.execute('INSERT INTO interval (id, userId, startDay, startHour, endDay, endHour) VALUES ('+
+								str(id) + ", " + 
+								str(userId) + ", " +
+								str(startDay) + ", " +
+								str(startHour) + ", " +
+								str(endDay) + ", " +
+								str(endHour) + ")")
+	db.commit()
 
-        return True
+def find_timezone(discordId):
+	cursor.execute('SELECT timezone FROM users WHERE discordId = '+str(discordId))
+	return cursor.fetchone()[0]
 
-    except sqlite3.DatabaseError:
-        return False
-
-
-def add_interval(discord_id, day, start_hour, end_hour):
-    try:
-        cursor.execute('INSERT INTO intervals (user_id, day, start_hour, end_hour) VALUES (?, ?, ?, ?)',
-                       (discord_id, day, start_hour, end_hour))
-        db.commit()
-
-        return True
-
-    except sqlite3.DatabaseError:
-        return False
-
-
-def get_user(discord_id):
-    """
-    :return: User (from models.py)
-    """
-    time_zone_id = cursor.execute("SELECT timezone from users WHERE discord_id = ?", (discord_id,)).fetchone()[0]
-    db_intervals = cursor.execute("SELECT id, day, start_hour, end_hour FROM intervals WHERE user_id = ?",
-                                  (discord_id,)).fetchall()
-
-    # Map the enums to actual values
-    time_zone = TIMEZONES[time_zone_id]
-    intervals = [Interval(i[0], DAYS[i[1]], i[2], i[3]) for i in db_intervals]
-
-    return User(discord_id, time_zone, intervals)
+def find_interval_user(userId):
+	cursor.execute('SELECT * FROM interval WHERE userId = ' + str(userId))
+	return cursor.fetchall()
 
 
-if __name__ == '__main__':
-    remove_tables()
-    create_tables()
-    add_user(1, 2)
-    add_interval(1, 2, 1, 5)
-    add_interval(1, 3, 5, 21)
-    print(get_user(1))
+if __name__ ==' __main__':
+	remove_tables()
+	create_tables()
+	add_user(1, 0)
+	add_user(2, 3)
+	add_interval(1, 1, 0, 9, 0, 12)
+	add_interval(1, 1, 1, 7, 1, 10)
+	add_interval(1, 2, 0, 4, 0, 9)
+	print(find_timezone(1))
+	print(find_interval_user(1))
+
 
