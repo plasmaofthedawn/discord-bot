05309907af51bc42b7ad892c9c9ad7d69c14955c
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2019-02-11 18:03:32 -0500

    added alter_timezone

--- main.py
+++ main.py
@@ -41,4 +41,23 @@ async def on_ready():
         print(client.user.id)
         print('------')
 
+def alter_timezone(start_day, start_hour, end_day, end_hour, timezone):
+
+    start_hour -= timezone
+    end_hour -= timezone
+
+    if start_hour < 0:
+        start_hour += 24
+        start_day -= 1
+        if start_day < 0:
+            start_day += 7
+    if end_hour  <0:
+        end_hour += 24
+        end_day -= 1
+        if end_day < 0:
+            end_day += 7
+
+    return start_day, start_hour, end_day, end_hour
+
+        
 client.run(config['token'])
