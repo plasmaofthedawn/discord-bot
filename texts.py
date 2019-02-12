7a69a499905763f145f15a78ff695ddf683ceb6f
Nick <dirisfaras@gmail.com>
2019-02-11 21:50:09 -0500

    File for longer strings, to keep main.py more neat

---
+++ texts.py
@@ -0,0 +1,6 @@
+help_txt = """set_time_zn <Ex: est or utc-5>
+add_interval <start day> <start time> <end day>(if different) <end time>
+upload_schedule <text file>
+show_schedule
+remove_interval <index, as displayed in "show_schedule">
+clear_schedule          wipes entire schedule"""
