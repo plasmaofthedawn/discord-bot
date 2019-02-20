bf37f61082aaf3e65b65682607fd23a64ed715de
Nick <dirisfaras@gmail.com>
2019-02-19 20:04:36 -0500

    Adding database and polling functions
    What I did, and how it works
    
        - Added ballots to the database
    
        - Created 'new_ballot' function within new class 'ballot_commands'. The user will be prompted to confirm their ballot, after being sent back all of the ballot details. User will confirm their ballot with command 'confirm_ballot'
    
        - Each ballot will be given a noun for a name, and an ID. Either of these can be used for interacting with a ballot
    
        - Nouns are stored in a .json file. This is only used for assigning nouns, and both the ID and noun are also stored inside the database.
    
        - Created command 'get_ballot' for displaying info for a ballot
    
        - Added texts for 'new_ballot help' and 'help'. And some other minor changes
    
        - 
    
    
    What I left incomplete/What I need to do
    
        - Need to add auto-deletion for pending ballots, after 24 hours
    
        - Need to display title in 'get_ballot'
    
        - Check if roles inputed are actual roles
    
    'svpc' mean See Votes Pre-Close, btw. Didn't want to take up all that space, so I shortened it

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from texts import *
 import database
 import timezone
 import io
+from random import shuffle
 
 startup = True
 client = discord.Client()
@@ -11,9 +12,20 @@ client = discord.Client()
 with open('config.json', 'r') as read_file:
     config = json.load(read_file)
 
+nouns = json.load(open("noun_ids.json", 'r'))
+
 rounding_warning = ("Keep in mind that your schedule will be trimmed if " +
                     "it doesn't fit into increments of " + str(config["interval_block_size"]) +
                     " minute(s)")
+
+pending_ballots = []
+votingIndicator = "channel"
+try:
+    if(config["voting_channel"] == ""):
+        votingIndicator = "trigger"
+except KeyError:
+    votingIndicator = "trigger"
+
 
 # storage for loaded intervals
 loaded_intervals = []
@@ -92,11 +104,22 @@ class Commands:
 
     @staticmethod
     async def add_interval(params, message):
+        if(len(params) > 1):
+            if("-" in params[1]):
+                tmp = params[1].split("-", 1)
+                params = [params[0], tmp[0], tmp[1]]
         if len(params) >= 3:
             if len(params) == 3:
                 params = [params[0], params[1], params[0], params[2]]
-            times = [convert_time(params[1]), convert_time(params[3])]
+            if(is_time(params[1]) and is_time(params[3])):
+                times = [convert_time(params[1]), convert_time(params[3])]
+            else:
+                await message.channel.send("One of your times is not a time")
+            tmp = [params[0], times[0], params[2], times[1]]
             tz = database.get_user(message.author.id)
+            if(validInterval(tmp) != 1):
+                await message.channel.send(validInterval(tmp))
+                return
             if not tz:
                 await message.channel.send("couldn't find your timezone in the database")
                 return
@@ -106,8 +129,8 @@ class Commands:
             try:
                 start_day, start_hour, end_day, end_hour = alter_timezone(int(params[0]), times[0],
                                                                           int(params[2]), times[1], tz)
-            except TypeError:
-                await message.channel.send("one of your inputs is not a time")
+            except:
+                await message.channel.send("Error processing interval")
                 return
 
             database.add_interval(message.author.id, start_day, end_day, start_hour, end_hour)
@@ -213,7 +236,9 @@ class Commands:
 @client.event
 async def on_message(message):
     # get params and run command
-    if message.content.startswith(config['trigger']):
+    channel = str(message.channel)
+    inMainChannel = channel == config["channel"] #True if in main channel
+    if (message.content.startswith(config['trigger'])) and inMainChannel:
         content = message.content.replace(config['trigger'], '', 1).split(' ')
         command = content[0]
         params = []
@@ -224,7 +249,250 @@ async def on_message(message):
             await getattr(Commands, command)(params, message)
         except AttributeError:
             await message.channel.send("'" + command + "' is not a command" +
-                                       ", use '!help' to get a list of commands")
+                                       ", use 'help' to get a list of commands")
+    else:
+        voting = 0
+        content = message.content
+        if(votingIndicator == "channel"):#in voting channel, and starts w/ trigger
+            if(message.content.startswith(config["trigger"])):
+                voting = channel == config["voting_channel"]
+            content = content.replace(config['trigger'], '', 1).split(' ')
+        elif(inMainChannel):#starts w/ voting_trigger and is in main channel
+            voting = message.content.startswith(config["voting_trigger"])
+            content = content.replace(config['voting_trigger'], '', 1).split(' ')
+        command = content[0]
+        params = []
+        if len(content) > 1:
+            params = content[1:]
+        
+        if(voting):
+            if(hasattr(ballot_commands, command)):
+                await getattr(ballot_commands, command)(params, message)
+            else:
+                await message.channel.send("'" + command + "' is not a voting " +
+                "command, use 'help' to get a list of commands")
+
+
+class ballot_commands:
+    @staticmethod
+    async def test(params, message):
+        await message.channel.send("You're voting!")
+    
+    @staticmethod
+    async def new_ballot(params, message):
+        # adds ballot to "pending_ballots" list, to be confirmed by user
+        global pending_ballots
+        send = message.channel.send
+        content = message.content
+        if(params == ['help']):
+            await send(new_ballot_help)
+            return
+        
+        #try:
+        prompt = content[12:].split('"', 2)[1]
+        covered = len(prompt[0]) + len(prompt[1]) #area covered already
+        Tmp = content[covered:].split("[", 1)[1].split("]")[0]
+        tmp = Tmp.split('"')
+        options = []
+        i = 1
+        while((i+1) < len(tmp)):
+            if((i % 2) > 0): # if odd
+                options.append(tmp[i])
+            else: #if even
+                if(not ("," in tmp[i])):
+                    if(i < (len(tmp)-1)):
+                        await send("Missing comma around '" +
+                        tmp[i-1] + tmp[i] + tmp[i+1] + "'")
+                    else:
+                        await send("Missing comma around '" +
+                        tmp[i-1] + tmp[i] + "'")
+                    return
+            i += 1
+        covered += len(content[covered:].split("[", 1)[0])
+        covered += len(Tmp) + 1
+        try:
+            write_in = int(content[covered:].split("write_in=", 1)[1][0])
+            svpc = int(content[covered:].split("svpc=", 1)[1][0])
+            # ^ see votes pre-close
+        except ValueError:
+            pass
+        
+        if((write_in != 1) & (write_in != 0)):
+            send("Invalid write_in value '" + str(write_in) + "'")
+            return
+        if((svpc != 1) & (svpc != 0)):
+            send("Invalid see_votes_pre-close(svpc) value '" + str(svpc) + "'")
+            return
+        try:
+            tmp = content[covered:].split("roles=[", 1)[1].split("]")[0]
+            tmp = tmp.split('"')
+        except IndexError:
+            await send("Error processing roles, see 'new_ballot help' for " +
+            "formating instruction")
+            return
+        roles = []
+        i = 1
+        while((i+1) < len(tmp)):
+            if((i % 2) > 0): # if odd
+                roles.append(tmp[i])
+            else: #if even
+                if(not ("," in tmp[i])):
+                    await send("Missing comma around '" +
+                    tmp[i-1] + tmp[i] + "'")
+                    return
+            i += 1
+        title = "None"
+        if('title="' in content[covered:]):
+            title = content[covered:].split('title="', 1)[1].split('"')[0]
+        """except Exception as e:
+            print(e)
+            await send("Error processing, please make sure you've properly " +
+            "formated everything. See 'new_ballot help'")
+            return"""
+        global nouns
+        keys = list(nouns.keys())
+        shuffle(keys)
+        i = 0
+        found = 0
+        ID = 0
+        while(i < len(keys)):
+            if(nouns[keys[i]] == ""):
+                found = 1
+                I = i
+            elif(ID <= int(nouns[keys[i]])):
+                ID = int(nouns[keys[i]]) + 1
+            i += 1
+        if(not found): #I doubt this will ever happen
+            await message.channel.send("The limit of nouns has been reached, " +
+            "contact the bot operator.")
+            return
+        ID = str(ID)
+        if(len(ID) == 1):
+            ID = "00" + ID
+        elif(len(ID) == 2):
+            ID = "0" + ID
+        nouns[keys[I]] = ID
+        
+        await send("Prompt: '" + prompt + "'\nOptions: ['" + "', '".join(options) +
+        "']\nWrite In: " + str(write_in) + "\nRoles: ['" + "', '".join(roles) +
+        "']\nTitle: '" + title + "'\nUse 'confirm_ballot' to confirm the ballot" +
+        ", if all of this information is correct. You have 24hours before " + 
+        "the ballot is auto-deleted\n\nBallot ID: '" + ID + "'\nBallot Name: '" +
+        keys[I] + "'")
+        tmp = {"noun": keys[I], "title": title, "author": message.author.id,
+        "prompt": prompt, "options": options, "write_in": write_in,
+        "svpc": svpc}
+        
+        pending_ballots.append(tmp)
+        
+        json.dump(nouns, open("nouns_ids.json", 'w'), indent=2)
+    
+    @staticmethod
+    async def confirm_ballot(params, message):
+        # adds ballot to database, after confirmation by user
+        global nouns
+        send = message.channel.send
+        keys = list(nouns.keys())
+        if(len(params) == 0):
+            await send("'confirm_ballot' requires either the ID or name of a ballot")
+            return
+        try:
+            int(params[0])
+            ID = params[0]
+            found = 0
+            i = 0
+            while(i < len(keys)):
+                if(nouns[keys[i]] == ID):
+                    found = 1
+                    noun = keys[i]
+                i += 1
+            if(not found):
+                await send("Failed to find ballot with ID '" + params[0] + "'")
+                return
+        except ValueError:
+            ID = nouns[params[0].lower()]
+            noun = params[0].lower()
+            if(ID == ""):
+                await send("'" + params[0].lower() + "' is not a valid ID and " +
+                "isn't a name of any ballot")
+                return
+        global pending_ballots
+        found = 0
+        i = 0
+        while(i < len(pending_ballots)):
+            if(pending_ballots[i]["noun"] == noun):
+                pb = pending_ballots[i] #pending ballot
+                found = 1
+            i += 1
+        if(not found):
+            await send("Ballot #" + ID + "/" + noun + " is not a pending ballot")
+            return
+        
+        author = message.author.id
+        if(author == pb["author"]):
+            if(database.add_ballot(ID, pb["noun"], pb["author"],
+            ", ".join(pb["options"]), pb["write_in"], pb["svpc"], pb["title"])
+            ):
+                await send("Ballot added to the database")
+            else:
+                await send("An error occurred while adding ballot to database")
+        else:
+            await send("Access Denied")
+    
+    @staticmethod
+    async def vote(params, message):
+        send = message.channel.send
+        if(len(params) == 0):
+            await send("'vote' requires 2 parameters, see 'help'")
+            return
+        
+    @staticmethod
+    async def get_ballot(params, message):
+        send = message.channel.send
+        if(len(params) == 0):
+            await send("'get_ballot' requires either the ballot's ID or name")
+            return
+        info = database.get_ballot(params[0])[0]
+        if(info == 0):
+            await send("'" + params[0] + "' ballot couldn't be found")
+            return
+        
+        tmp = info[6].split(", ")
+        votes = ""
+        i = 0
+        while(i < len(tmp)):
+            votes = votes + str(i) + "- " + tmp[i] + "  "
+            i += 1
+        
+        tmp = info[3].split(", ")
+        options = ""
+        i = 0
+        while(i < len(tmp)):
+            options = options + str(i) + ") '" + tmp[i] + "', "
+            i += 1
+        options = options[:-2]
+        votes = votes[:-2]
+        Open = bool(int(info[8]))
+        svpc = bool(int(info[5]))
+        if((not svpc) and Open):
+            votes = "None can view votes until ballot has closed"
+        
+        status = "Closed"
+        if(Open):
+            status = "Open"
+        
+        author = await client.get_user_info(info[2])
+        author = author.name + "#" + author.discriminator
+        await send("Title: " + info[9] + "\nID: " + info[0] + "\nName: " +
+        info[1] + "\nCreator: " + author + "\nOptions: " + options +
+        "\nWrite-in allowed: " + to_bool(info[4]) +
+        "\nView votes pre-close allowed:" + str(svpc) + "\nVotes:\n" + votes +
+        "\nStatus: " + status)
+    
+    @staticmethod
+    async def help(params, message):
+        await message.channel.send(voting_help)
+
 
 
 @client.event
@@ -236,6 +504,8 @@ async def on_ready():
         print(client.user.id)
         print('------')
 
+
+to_bool = lambda i: str(bool(int(i))) # converts string to bool, Ex "0" to "False"
 
 def alter_timezone(start_day, start_hour, end_day, end_hour, tz):
     start_hour -= tz
@@ -262,6 +532,31 @@ def alter_timezone(start_day, start_hour, end_day, end_hour, tz):
         if end_day > 6:
             end_day -= 7
     return start_day, start_hour, end_day, end_hour
+
+
+def validInterval(interval):
+    try:
+        startD = int(interval[0])
+        endD = int(interval [2])
+    except TypeError:
+        return "One of your days is not a number"
+    startT = interval[1]
+    endT = interval[3]
+    if((startD < 0) or (startD > 6)):
+        return "Invalid start day"
+    elif((endD < 0) or (endD > 6)):
+        return "Invalid end day"
+    elif((startT >= 24) or (startT < 0)):
+        return "Invalid start time"
+    elif((endT >= 24) or (endT < 0)):
+        return "Invalid end time"
+    elif(((endT < startT) and (endD == startD)) or (endD < startD)):
+        return ("I don't care how clocks work where you're from, here in " +
+        "America clocks go forward!")
+    elif((endT == startT) and (endD == startD)):
+        return "Your end and start times are the same"
+    else:
+        return True
 
 
 def convert_time(time_):
@@ -307,9 +602,8 @@ def process(schedule, user):
     blocks = tmp.replace(" ", "").lower().split("}\n")
     blocks[-1] = blocks[-1].replace("\n}", "")
     i = 0
-    failed = 0
     entries = [[]]
-    while (i < len(blocks)) and (not failed):
+    while(i < len(blocks)):
         j = 0
         day = -1
         day_start = -1
@@ -329,44 +623,48 @@ def process(schedule, user):
             if day_end > 6:
                 day_end -= 7
             if (day_start == -1) or (day_end == -1):
-                print("upload_schedule processing failure at " + blocks[i])
-                failed = 1
+                return 0
         else:
             while j < 14:
                 if blocks[i].split("{")[0] == days[j]:
                     day = j
                 j += 1
             if day == -1:
-                print("upload_schedule processing failure at " + blocks[i])
                 i = len(blocks)
-                failed = 1
+                return 0
             elif day > 6:
                 j -= 7
-        lines = blocks[i].split("\n")[1:]
+        try:
+            lines = blocks[i].split("\n")[1:]
+        except:
+            return 0
         entries.append([])
         j = 0
         next_day = 0
-        while (j < len(lines)) and ("-" in lines[j]) and (not failed):
+        while (j < len(lines)) and ("-" in lines[j]):
             if "--" in lines[j]:
                 lines[j] = lines[j].replace("--", "-")
                 next_day = 1
             start, end = lines[j].split("-")
             if not is_time(start):
-                print(start + " is not a time")
-                failed = 1
+                return 0
             if not is_time(end):
-                print(end + " is not a time")
-                failed = 1
-            start = convert_time(start)
-            end = convert_time(end)
+                return 0
+            if(is_time(start) and is_time(end)):
+                start = convert_time(start)
+                end = convert_time(end)
+                if(start <= end):
+                    return 0
+            else:
+                return 0
             entries[i].append((start, end, next_day, day, day_start, day_end))
             next_day = 0
             j += 1
         i += 1
     i = 0
-    while (i < len(blocks)) and (not failed):
+    while i < len(blocks):
         j = 0
-        if not failed:
+        if 1:
             tz = user.timezone
             while j < len(entries[i]):
                 day = entries[i][j][3]
@@ -396,10 +694,7 @@ def process(schedule, user):
                             day = 0
                 j += 1
         i += 1
-    if failed:
-        return 0
-    else:
-        return 1
+    return 1
 
 
 async def attachment_to_bytes(attachment):
--- texts.py
+++ texts.py
@@ -1,9 +1,12 @@
-help_txt = """set_time_zn <Ex: est or utc-5>
+help_txt = """Scheduling Help
+
+set_time_zn <Ex: est or utc-5>
 add_interval <start day> <start time> <end day>(if different) <end time>
 upload_schedule <text file>
 show_schedule
 remove_interval <index, as displayed in "show_schedule">
-clear_schedule          wipes entire schedule"""
+clear_schedule          wipes entire schedule
+authors          returns a list authors"""
 
 
 upload_schedule_example = """Mon{
@@ -22,3 +25,25 @@ Sun-Sat{
 Note: You can put your schedule in either 24hour format, 12 hours format, or a mix of both. And if multiple days have the same interval, simply put a hyphen between the first and last days(Ex: Wed-Fri{...) In addition, you may use doube hypens(--) for a time that spans over to the next day. Btw, spaces are ignored"""
 
 upload_schedule_help = """upload_schedule > Upload a text file with your schedule, to see an example call "!upload_schedule example\""""
+
+new_ballot_help = """new_ballot "<prompt>" ["<option1>", ect..] write_in=<bool> (0 or 1) roles=["<role1>", ect..](roles to limit vote to, roles are listed as text, not meantions) svpc=<bool>(see votes pre-close) title=<title>(optional, title of ballot)
+
+Example: new_ballot "What is the best cake?" ["Cookie Cake", "Cake"] write_in=0 roles=["everyone"] svpc=1 title="Best Cake" 
+
+Prompt has to go first, then options, but everything else can be in whatever order"""
+
+voting_help = """Voting Help
+
+new_ballot          See 'new_ballot help' for instruction
+close_ballot <ballot name or ID>    closes a ballot
+extend_time <ballot name or ID> <Days:Hours:Minutes>    pushes back scheduled closing time
+vote <ballot name or ID> <option #>(if writing in, put vote within double quotes, Ex: "pie")
+current_ballots          lists currently open ballots
+my_ballots          lists all of your ballots, open and closed
+closed_ballots          lists all closed ballots
+ballot_options <ballot name or ID>    prints a list of all options for a ballot
+get_ballot <ballot name or ID>    prints all information for a ballot
+add_options <ballot name or ID> [<option 1>, etc..]    adds option(s) to ballot
+add_roles <ballot name or ID> [<role 1>, etc..]    adds role(s) to those allowed to vote
+allow_write_in <ballot name or ID>    enables writing in, for a ballot
+authors          returns a list authors"""
